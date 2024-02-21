c669d1d6a4cac9c812083215eb69cca3c00f0e1d
Ljupcho.Taskov-FE11 <ljupchotaskov1@gmail.com>
2024-02-21 15:14:13 +0100

    Bike shop challenge added

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/Header";
 import Filters from "./components/Filters";
